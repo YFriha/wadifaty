5ed162e18438f66b419364762bb7c15c88d3b048
YFriha <yassir.friha@gmail.com>
2026-03-23 16:51:34 +0100

    fix django vercel routing

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api/';
+const baseURL = import.meta.env.VITE_API_URL || '/api/';
 
 const axiosInstance = axios.create({ baseURL });
 
